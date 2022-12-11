cbbbae560422af5aa55b39cd14c4c215590ab7b3
Adrian Fernandez <adrianxtuu@gmail.com>
2022-12-11 16:50:03 -0400

    [UPDATE]

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,10 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType, ChartEvent } from 'chart.js';
+
 
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html',
 })
 export class DonaComponent implements OnInit {
+
+
+  // Doughnut
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [350, 450, 100] },
+    ],
+
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    // console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    // console.log(event, active);
+  }
+
+
+
 
   constructor() { }
 
