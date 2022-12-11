d9927ace9cd8d8ca46f03cb80278119f1f0f7735
Adrian Fernandez <adrianxtuu@gmail.com>
2022-12-11 16:35:12 -0400

    [UPDATE]

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild,  } from '@angular/core';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
 
 
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
@@ -9,7 +9,12 @@ import { BaseChartDirective } from 'ng2-charts';
   selector: 'app-grafica-barra',
   templateUrl: './grafica-barra.component.html',
 })
-export class GraficaBarraComponent  {
+export class GraficaBarraComponent implements OnInit {
+
+
+  @Input() horizontal: boolean = false
+
+
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
@@ -21,15 +26,8 @@ export class GraficaBarraComponent  {
   public barChartType: ChartType = 'bar';
 
 
-  public barChartData: ChartData<'bar'> = {
-    labels: ['2022', '2023', '2024', '2025', '2026', '2027', '2028'],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#41F248', hoverBackgroundColor: '#FFFFFF'},
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#44FCDA' , hoverBackgroundColor: '#FFFFFF'},
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series C', backgroundColor: '#4990E6' , hoverBackgroundColor: '#FFFFFF'},
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series D', backgroundColor: '#743CFF' , hoverBackgroundColor: '#FFFFFF'},
-      
-    ]
+  @Input() barChartData: ChartData = {
+    datasets: []
   };
 
   // events
@@ -41,52 +39,14 @@ export class GraficaBarraComponent  {
     // console.log(event, active);
   }
 
-  public randomize(): void {
 
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
+  constructor() { }
 
-    ];
+  ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartType = 'doughnut'
+    }
 
-    this.barChartData.datasets[1].data = [
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-
-    ];
-    this.barChartData.datasets[2].data = [
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-
-    ];
-    this.barChartData.datasets[3].data = [
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-
-    ];
-
-
-    this.chart?.update();
   }
 
 }
--- src/app/graficas/pages/barras-doubles/barras-doubles.component.ts
+++ src/app/graficas/pages/barras-doubles/barras-doubles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData } from 'chart.js';
+
 
 @Component({
   selector: 'app-barras-doubles',
@@ -6,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarrasDoublesComponent implements OnInit {
 
+
+  proveedoresData: ChartData = {
+
+    labels: ['2021', '2022', '2023', '2024', '2025'],
+    datasets: [
+      { data: [100, 200, 300, 400, 500], label: 'Vendedor A' },
+      { data: [50, 250, 30, 450, 200], label: 'Vendedor B' },
+
+    ]
+
+
+  }
+
+
+
+  productoData: ChartData = {
+
+    labels: ['2021', '2022', '2023', '2024', '2025'],
+    datasets: [
+      { data: [200, 300, 400, 300, 100], label: 'Carros', backgroundColor: 'blue' },
+
+    ]
+  }
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
 
 }
