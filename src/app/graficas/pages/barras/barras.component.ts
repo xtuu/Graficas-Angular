b281ecf1a40aae2e6b58225d0eda2cc3fa36b302
Adrian Fernandez <adrianxtuu@gmail.com>
2022-12-11 14:02:48 -0400

    [UPDATE]

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -2,73 +2,88 @@ import { Component, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
-import DataLabelsPlugin from 'chartjs-plugin-datalabels';
+
 
 @Component({
   selector: 'app-barras',
   templateUrl: './barras.component.html',
 })
 export class BarrasComponent {
-  
+
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
   public barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      x: {},
-      y: {
-        min: 10
-      }
-    },
-    plugins: {
-      legend: {
-        display: true,
-      },
-      datalabels: {
-        anchor: 'end',
-        align: 'end'
-      }
-    }
   };
+
+
   public barChartType: ChartType = 'bar';
-  
+
 
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    labels: ['2022', '2023', '2024', '2025', '2026', '2027', '2028'],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#41F248', hoverBackgroundColor: '#FFFFFF'},
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#44FCDA' , hoverBackgroundColor: '#FFFFFF'},
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series C', backgroundColor: '#4990E6' , hoverBackgroundColor: '#FFFFFF'},
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series D', backgroundColor: '#743CFF' , hoverBackgroundColor: '#FFFFFF'},
+      
     ]
   };
 
   // events
   public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-    console.log(event, active);
+    // console.log(event, active);
   }
 
   public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-    console.log(event, active);
+    // console.log(event, active);
   }
 
   public randomize(): void {
-    // Only Change 3 values
+
     this.barChartData.datasets[0].data = [
       Math.round(Math.random() * 100),
-      59,
-      80,
       Math.round(Math.random() * 100),
-      56,
       Math.round(Math.random() * 100),
-      40];
-      this.barChartData.datasets[1].data = [
-        Math.round(Math.random() * 100),
-        59,
-        80,
-        Math.round(Math.random() * 100),
-        56,
-        Math.round(Math.random() * 100),
-        40];
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+
+    ];
+
+    this.barChartData.datasets[1].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+
+    ];
+    this.barChartData.datasets[2].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+
+    ];
+    this.barChartData.datasets[3].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+
+    ];
+
 
     this.chart?.update();
   }
